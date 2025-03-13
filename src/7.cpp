d00a0e1c5b2ff5c052ecc616793e8c9cdb044664
elijury454 <elijury454@gmail.com>
2025-03-13 11:33:37 +0000

    Add 7.cpp

---
+++ src/7.cpp
@@ -0,0 +1,12 @@
+// A function that returns the sum of two numbers
+int sum(int num1, int num2) {
+  return num1 + num2;
+}
+
+// Driver code to test the above function
+int main() {
+  int num1 = 5;
+  int num2 = 7;
+  int result = sum(num1, num2);
+  cout << "Sum of " << num1 << " and " << num2 << " is: " << result << endl;
+}
