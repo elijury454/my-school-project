ec9bace954fe282e90c8f4c8a2023497ab58de1a
elijury454 <elijury454@gmail.com>
2025-03-16 12:04:36 +0000

    Add 10.cpp

---
+++ src/10.cpp
@@ -0,0 +1,9 @@
+int main() {
+  // A random number between 1 and 100
+  int randNum = (rand() % 100) + 1;
+
+  // Print the random number to the console
+  cout << "Random number: " << randNum << endl;
+
+  return 0;
+}
