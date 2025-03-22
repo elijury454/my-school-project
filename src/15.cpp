343aead57ea2ecff2e07d4b17b13992f4d1925e1
elijury454 <elijury454@gmail.com>
2025-03-22 15:52:48 +0000

    Add 15.cpp

---
+++ src/15.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 5;
+    double pi = 3.14159265358979323846;
+    cout << "The value of π is: " << pi << endl;
+    cout << "The value of num is: " << num << endl;
+
+    return 0;
+}
