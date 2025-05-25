be04f24c98e3305fe2a3a220b9905ea535453e60
elijury454 <elijury454@gmail.com>
2025-05-25 17:53:17 +0000

    Add 41.cpp

---
+++ src/41.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int a = 5;
+    std::cout << "Value of a: " << a << std::endl;
+
+    double b = 3.14;
+    std::cout << "Value of b: " << b << std::endl;
+
+    return 0;
+}
